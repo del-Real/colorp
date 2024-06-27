394af15890c8d0d6fd0f0e936c8bcc3d9ef27e8a
Alberto del Real <66703149+del-Real@users.noreply.github.com>
2024-06-27 02:10:26 +0200

    README updated

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # colorp
+
 Simple color picker for Linux
+
+Press [Q] or [Esc] to exit the application
+
+## Dependencies
+
+### Debian / Ubuntu
+
+```console
+$ sudo apt-get install libx11-dev
+```
+
+## Run the application
+
+```console
+$ cc -o colorp colorp.c
+$ ./colorp
+```
--- colorp.c
+++ colorp.c
@@ -1,12 +1,15 @@
-#include <stdio.h>
+// colorp | A simple color picker for Linux
+// Copyright (c) 2024 Alberto del Real
+// This code is licensed under the MIT License.
+
 #include <X11/Xlib.h>
 #include <X11/Xutil.h>
+#include <stdio.h>
 #include <unistd.h>
 
 #define SQUARE_SIZE 100
 
-int main()
-{
+int main() {
     Display *display;
     Window root, win;
     Window dummy_win;
@@ -18,6 +21,7 @@ int main()
     unsigned int mask;
     int shouldClose = 1; // Flag to control the loop
     unsigned long pixel;
+
     XImage *image;
     XColor color;
     XColor prevColor;
@@ -31,8 +35,8 @@ int main()
     root = RootWindow(display, screen);
 
     // Create window
-    win = XCreateSimpleWindow(display, root, 10, 10, SQUARE_SIZE, SQUARE_SIZE, 1,
-                              BlackPixel(display, screen), WhitePixel(display, screen));
+    win = XCreateSimpleWindow(display, root, 10, 10, SQUARE_SIZE, SQUARE_SIZE, 1, BlackPixel(display, screen),
+                              WhitePixel(display, screen));
 
     // Select input events for the window
     XSelectInput(display, win, ExposureMask | KeyPressMask);
@@ -46,19 +50,16 @@ int main()
     printf("==============================================\n");
     printf("    colorp by Alberto del Real (RedSantar)\n");
     printf("----------------------------------------------\n");
-    printf(" Press [Q] or [ESC] to exit the application\n");
+    printf(" Press [Q] or [Esc] to exit the application\n");
     printf("==============================================\n\n");
 
     // Main loop
-    while (shouldClose)
-    {
+    while (shouldClose) {
         // Query the pointer/mouse cursor
-        if (XQueryPointer(display, root, &dummy_win, &dummy_win, &x, &y, &dummy_int, &dummy_int, &mask))
-        {
+        if (XQueryPointer(display, root, &dummy_win, &dummy_win, &x, &y, &dummy_int, &dummy_int, &mask)) {
             // Get the image at the pointer's coordinates
             image = XGetImage(display, root, x, y, 1, 1, AllPlanes, ZPixmap);
-            if (image != NULL)
-            {
+            if (image != NULL) {
                 pixel = XGetPixel(image, 0, 0);
                 color.pixel = pixel;
                 XQueryColor(display, DefaultColormap(display, screen), &color);
@@ -69,8 +70,7 @@ int main()
                 // Fill the window with the color
                 XFillRectangle(display, win, gc, 0, 0, SQUARE_SIZE, SQUARE_SIZE);
 
-                if (color.pixel != prevColor.pixel)
-                {
+                if (color.pixel != prevColor.pixel) {
                     // Print color and Hex code
                     printf("\033[38;2;%d;%d;%dm██████████\033[0m", color.red >> 8, color.green >> 8, color.blue >> 8);
                     printf(" HEX | #%02x%02x%02x\n", color.red >> 8, color.green >> 8, color.blue >> 8);
@@ -78,32 +78,20 @@ int main()
                 prevColor.pixel = pixel;
                 XDestroyImage(image);
             }
-            else
-            {
-                fprintf(stderr, "Failed to get image\n");
-            }
-        }
-        else
-        {
-            fprintf(stderr, "Failed to query pointer\n");
         }
 
         // Event handler
-        while (XPending(display))
-        {
+        while (XPending(display)) {
             XNextEvent(display, &event);
-            if (event.type == Expose)
-            {
+            if (event.type == Expose) {
                 // Repaint the window with the current color
                 XFillRectangle(display, win, gc, 0, 0, SQUARE_SIZE, SQUARE_SIZE);
             }
 
             // Close app is Q_KEY or ESC is pressed
-            else if (event.type == KeyPress)
-            {
+            else if (event.type == KeyPress) {
                 KeySym keysym = XLookupKeysym(&event.xkey, 0);
-                if (keysym == XK_q || keysym == XK_Escape)
-                {
+                if (keysym == XK_q || keysym == XK_Escape) {
 
                     shouldClose = 0;
                 }
